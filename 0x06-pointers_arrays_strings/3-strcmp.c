40a87d22b2b5c76648b637b67e787f34667f7090
eloquentjohn <agadajohnnie@gmail.com>
2023-09-21 08:07:44 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,25 @@
 #include "main.h"
+
 /**
- * _strcmp - this is function program name
- * @s1: input value
- * @s2: input value 
- * Return: s1[c] = s2[c]
- */
+  * _strcmp - The main function
+  *
+  * @s1: Function parameter
+  *
+  * @s2: Function parameter
+  *
+  * Return: s1, s2
+  */
 int _strcmp(char *s1, char *s2)
 {
-	int c;
+int c = 0;
 
-	c = 0;
-	while (s1[c] != '\0' && s2[c] != '\0')
+while (s1[c] != '\0' && s2[c] != '\0')
+{
+	if (s1[c] != s2[c])
 	{
-		if (s1[c] != s2[c])
-		{
-			return (s1[c] = s2[c]);
-		}
-		c++;
+	return (s1[c] - s2[c]);
 	}
-	return (0);
+	c++;
+	}
+return (0);
 }
