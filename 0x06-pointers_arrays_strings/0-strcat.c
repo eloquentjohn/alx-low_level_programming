cef8923428e50b182e66ef3b1f4dbd8493ba7366
eloquentjohn <agadajohnnie@gmail.com>
2023-09-20 23:09:34 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * _strcat - function program
+ * @dest: input value 
+ * @src: input value
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c;
+	int j;
+
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[c] =  src[j];
+		c++;
+		j++;
+	}
+
+	dest[c] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my C - More pointers, arrays and strings readme project
