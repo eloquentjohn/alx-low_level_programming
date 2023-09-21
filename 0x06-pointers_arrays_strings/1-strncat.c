a2b213bbe1b284e3f644013988e55e6792f28b74
eloquentjohn <agadajohnnie@gmail.com>
2023-09-21 08:29:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
 /**
- * _strncat - program function name 
- * it will use at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value 
- * Return: dest
- */
+  * _strncat  - the main function
+  *
+  * @n: Function parameter
+  *
+  * @src: Function parameter
+  *
+  * @dest: Function parammeter
+  *
+  * Return: dest
+  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int c;
-	int j;
-
+	int c, r;
+	
 	c = 0;
+	r = 0;
+	
 	while (dest[c] != '\0')
 	{
 	c++;
 	}
-	j = 0;
-	while (j < n && src[j] != '\0')
+	while (r < n && src[r] != '\0')
 	{
-	dest[c] = src[j];
+	dest[c] = src[r];
 	c++;
-	j++;
+	r++;
 	}
 	dest[c] = '\0';
 	return (dest);
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _strncpy - The main function of the code
+  *
+  * @src: Function parameter
+  *
+  * @dest: Function parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 0;
+		
+	while (c < n && src[c] != '\0')
+	{
+	dest[c] = src[c];
+	c++;
+	}
+	while (c < n)
+	{
+	dest[c] = '\0';
+	c++;
+	}
+	return (dest);
+	}
+}
