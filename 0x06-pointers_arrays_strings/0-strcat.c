9233a428bea9075e5a0b24822bfe3fe93e088701
eloquentjohn <agadajohnnie@gmail.com>
2023-09-21 23:59:52 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,33 @@
 #include "main.h"
 
 /**
-* _strcat - function program
-* @dest: input value 
-* @src: input value
-* Return: 0
-*/
+  *_strcat - main function of the prototype
+  *
+  * @src: Concatenation parameter
+  *
+  * @dest: Concatenation parameter
+  *
+  * Return: dest
+  */
+
 char *_strcat(char *dest, char *src)
 {
-	int c;
-	int j;
+int c, r;
 
-	c = 0;
-	while (dest[c] != '\0')
-	{
-		c++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[c] =  src[j];
-		c++;
-		j++;
-	}
+c = 0;
+r = 0;
 
-	dest[c] = '\0';
-	return (dest);
+while (dest[c] != '\0')
+{
+	c++;
+}
+
+while (src[r] != '\0')
+{
+	dest[c] = src[r];
+	c++;
+	r++;
+}
+dest[c] = '\0';
+return (dest);
 }
